1dc42fa01153b2f852c8fe56e0c2bd3d982a3f0a
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-10 08:58:33 -0700

    added laser-cannon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DeathSquareComponent } from './death-square/death-square.component';
 import { TurretComponent } from './turret/turret.component';
+import { LaserCannonComponent } from './laser-cannon/laser-cannon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeathSquareComponent,
-    TurretComponent
+    TurretComponent,
+    LaserCannonComponent
   ],
   imports: [
     BrowserModule
