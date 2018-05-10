c9229714421c55b42eb5a20a7649ea70a8f4898b
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-10 08:53:55 -0700

    added turret components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DeathSquareComponent } from './death-square/death-square.component';
+import { TurretComponent } from './turret/turret.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeathSquareComponent,
+    TurretComponent
   ],
   imports: [
     BrowserModule
